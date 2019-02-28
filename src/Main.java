f10796381f5626e9f002b26c139b8a17ab2cb3d2
lcb42 <blatherwick.lucy@gmail.com>
2019-02-28 15:38:23 +0000

    begun to set up database

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println();
+
+        // Do the thing
+        //set up an instance of a database
+        db db = new db();
     }
 }
---
+++ src/db.java
@@ -0,0 +1,9 @@
+import java.util.Properties;
+
+public class db {
+    //sqlite database to handle the ratings data
+
+    private final String url = "jdbc:postgresql://localhost/dvdrental";
+    private final String user = "postgres";
+    private final String password = "superman";
+}
